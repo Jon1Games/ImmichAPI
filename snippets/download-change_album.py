467728ef83563cf6dc1e96e208dda1d22c7112c1
Jon1Games <118659471+Jon1Games@users.noreply.github.com>
2025-04-26 23:56:31 +0200

    add fuction to run somethere else

--- snippets/download-change_album.py
+++ snippets/download-change_album.py
@@ -13,26 +13,43 @@ source_album_name = os.getenv("SOURCE_ALBUM_NAME")
 destination_album_name = os.getenv("DESTINATION_ALBUM_NAME")
 destination_folder = os.getenv("DESTINATION_FOLDER")
 
-immich = ImmichAPI(serverURL, apiKey)
+def run(
+        serverURL: str,
+        apiKey: str,
+        image_count: int,
+        source_album_name: str,
+        destination_album_name: str,
+        destination_folder: str
+        ):
+    immich = ImmichAPI(serverURL, apiKey)
 
-source_album = immich.getAlbumByName(source_album_name)
-destination_album = immich.getAlbumByName(destination_album_name)
-assets = immich.downloadRandomAssetFromAlbum(source_album["id"], count=image_count)
+    source_album = immich.getAlbumByName(source_album_name)
+    destination_album = immich.getAlbumByName(destination_album_name)
+    assets = immich.downloadRandomAssetFromAlbum(source_album["id"], count=image_count)
 
-if not os.path.exists(destination_folder):
-    os.makedirs(destination_folder)
-if destination_folder != "" or destination_folder is None:
-    destination_folder = destination_folder + "/"
+    if not os.path.exists(destination_folder):
+        os.makedirs(destination_folder)
+    if destination_folder != "" or destination_folder is None:
+        destination_folder = destination_folder + "/"
 
-count = 0
-for asset in assets:
-    while True:
-        count = count + 1
-        image_path = destination_folder + "asset-" + str(count) + ".jpg"
-        if not os.path.exists(image_path):
-            break
+    count = 0
+    for asset in assets:
+        while True:
+            count = count + 1
+            image_path = destination_folder + "asset-" + str(count) + ".jpg"
+            if not os.path.exists(image_path):
+                break
 
-    with open(image_path, "wb") as file:
-        file.write(asset["data"].read())
+        with open(image_path, "wb") as file:
+            file.write(asset["data"].read())
 
-immich.moveToOtherAlbum(sourceAlbumId=source_album["id"], destinationAlbumId=destination_album["id"], assetIds=[asset["id"] for asset in assets])
+    immich.moveToOtherAlbum(sourceAlbumId=source_album["id"], destinationAlbumId=destination_album["id"], assetIds=[asset["id"] for asset in assets])
+
+run(
+    serverURL=serverURL,
+    apiKey=apiKey,
+    image_count=image_count,
+    source_album_name=source_album_name,
+    destination_album_name=destination_album_name,
+    destination_folder=destination_folder
+)
